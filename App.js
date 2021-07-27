8fb5427798e56674b7aa25c0a17c38e7a16dff2e
Theo-Rokas <T.Rokas@hotmail.com>
2021-07-27 12:51:40 +0300

    First Commit

---
+++ App.js
@@ -0,0 +1,84 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import 'react-native-gesture-handler'
+import React, { useEffect } from 'react';
+
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+
+import Navigation from './src/navigation'
+import { withAuthenticator } from 'aws-amplify-react-native'
+import { Auth, API, graphqlOperation} from 'aws-amplify'
+import { createUser } from './src/graphql/mutations'
+import { getUser } from './src/graphql/queries'
+
+const randomImages = [
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg',
+];
+
+const getRandomImage = () => {
+  return randomImages[Math.floor(Math.random() * randomImages.length)]
+}
+
+const App = () => {
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser({ bypassCache: true })
+      
+      if(!userInfo) {
+        return
+      }
+
+      const getUserResponse = await API.graphql(
+        graphqlOperation(
+          getUser,
+          { id: userInfo.attributes.sub }
+        )
+      )
+
+      if(getUserResponse.data.getUser) {
+        console.log("User already exists in database");
+        return
+      }
+
+      const newUser = {
+        id: userInfo.attributes.sub,
+        username: userInfo.username,
+        email: userInfo.attributes.email,
+        imageUri: getRandomImage()
+      }
+
+      await API.graphql(
+        graphqlOperation(
+          createUser,
+          { input: newUser }
+        )
+      )
+    }
+
+    fetchUser()
+  }, [])
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+
+      <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
+        <Navigation />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default withAuthenticator(App)
---
+++ data/posts.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        id: 'p1',
+        videoUri: 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4',
+        user: {
+            id: 'u1',
+            username: 't.rokas',
+            imageUri: 'https://scontent.fath4-2.fna.fbcdn.net/v/t1.6435-9/100797371_2696869260594000_2112678006127329280_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=01dHYLqdBQwAX8AFSUh&_nc_ht=scontent.fath4-2.fna&oh=854f61638a2ce8955ff0f9a841e7c343&oe=60DFFFC6'
+        },
+        description: 'Earth is flat',
+        songName: 'NF - The Search',
+        songImage: 'https://scontent.fath4-2.fna.fbcdn.net/v/t1.6435-9/100797371_2696869260594000_2112678006127329280_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=01dHYLqdBQwAX8AFSUh&_nc_ht=scontent.fath4-2.fna&oh=854f61638a2ce8955ff0f9a841e7c343&oe=60DFFFC6',
+        likes: 123,
+        comments: 12,
+        shares: 44
+    },
+    {
+        id: 'p2',
+        videoUri: 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4',
+        user: {
+            id: 'u2',
+            username: 'c.rokas',
+            imageUri: 'https://scontent.fath4-2.fna.fbcdn.net/v/t1.6435-9/100797371_2696869260594000_2112678006127329280_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=01dHYLqdBQwAX8AFSUh&_nc_ht=scontent.fath4-2.fna&oh=854f61638a2ce8955ff0f9a841e7c343&oe=60DFFFC6'
+        },
+        description: 'Earth is flat',
+        songName: 'NF - The Search',
+        songImage: 'https://scontent.fath4-2.fna.fbcdn.net/v/t1.6435-9/100797371_2696869260594000_2112678006127329280_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=01dHYLqdBQwAX8AFSUh&_nc_ht=scontent.fath4-2.fna&oh=854f61638a2ce8955ff0f9a841e7c343&oe=60DFFFC6',
+        likes: 123,
+        comments: 12,
+        shares: 44
+    },
+    {
+        id: 'p3',
+        videoUri: 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4',
+        user: {
+            id: 'u3',
+            username: 'm.rokas',
+            imageUri: 'https://scontent.fath4-2.fna.fbcdn.net/v/t1.6435-9/100797371_2696869260594000_2112678006127329280_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=01dHYLqdBQwAX8AFSUh&_nc_ht=scontent.fath4-2.fna&oh=854f61638a2ce8955ff0f9a841e7c343&oe=60DFFFC6'
+        },
+        description: 'Earth is flat',
+        songName: 'NF - The Search',
+        songImage: 'https://scontent.fath4-2.fna.fbcdn.net/v/t1.6435-9/100797371_2696869260594000_2112678006127329280_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=01dHYLqdBQwAX8AFSUh&_nc_ht=scontent.fath4-2.fna&oh=854f61638a2ce8955ff0f9a841e7c343&oe=60DFFFC6',
+        likes: 123,
+        comments: 12,
+        shares: 44
+    },
+    {
+        id: 'p4',
+        videoUri: 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4',
+        user: {
+            id: 'u4',
+            username: 'i.rokas',
+            imageUri: 'https://scontent.fath4-2.fna.fbcdn.net/v/t1.6435-9/100797371_2696869260594000_2112678006127329280_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=01dHYLqdBQwAX8AFSUh&_nc_ht=scontent.fath4-2.fna&oh=854f61638a2ce8955ff0f9a841e7c343&oe=60DFFFC6'
+        },
+        description: 'Earth is flat',
+        songName: 'NF - The Search',
+        songImage: 'https://scontent.fath4-2.fna.fbcdn.net/v/t1.6435-9/100797371_2696869260594000_2112678006127329280_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=01dHYLqdBQwAX8AFSUh&_nc_ht=scontent.fath4-2.fna&oh=854f61638a2ce8955ff0f9a841e7c343&oe=60DFFFC6',
+        likes: 123,
+        comments: 12,
+        shares: 44
+    }
+]
